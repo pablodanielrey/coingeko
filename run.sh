129e95bcfd7f980e1882633b69bc3f92f3064b2c
Pablo Daniel Rey <pablo.rey@epicio.com>
2025-01-27 10:58:04 -0300

    updated the repository in hub.docker.com

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -ti -e COIN_IDS="ripple,stellar,cardano,solana,hedera-hashgraph,ethereum,radium" -p 1280:8080 coingeko-server 
+docker run --rm -ti -e COIN_IDS="ripple,stellar,cardano,solana,hedera-hashgraph,ethereum,radium" -p 1280:8080 pablodanielrey/coingeko-server 
